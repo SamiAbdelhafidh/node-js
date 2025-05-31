f68e84069257eaf5b62e8068fbafd96d95119c7b
SamiAbdelhafidh <louzaysami@gmail.com>
2025-05-31 23:41:50 +0100

    !!

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.write("welcome to my server\n");
-  res.write("Sami benAbdelhafidh\n");
+  res.write("Sami benAbdelhafidh\n ");
   res.end("done");
 
 
