f998b5be703ae49e8d87eb135f47e528f54ce088
SamiAbdelhafidh <louzaysami@gmail.com>
2025-06-01 15:08:50 +0100

    error in statusCode all page =200 !!!

--- server.js
+++ server.js
@@ -2,10 +2,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  
   res.setHeader('Content-Type', 'text/html');
-  res.write("welcome to my server\n");
   res.write("Sami benAbdelhafidh\n ");
+
+  if (req.url == '/home') {
+    res.statusCode=200
+    res.write("<h1>Welcome to the home page</h1>")
+  }
+  else if (req.url == '/about') {
+    res.statusCode=200
+    res.write("<h1>Welcome to the about page</h1>")
+  }
+  else if (req.url == '/contact') {
+    res.statusCode=404
+    res.write("<h1>Welcome to the contact page</h1>")
+  }
+  else {
+    res.statusCode = 404
+    res.write("<h1>404 Not Found</h1>")
+  }
+  
   res.end("done");
 
 
